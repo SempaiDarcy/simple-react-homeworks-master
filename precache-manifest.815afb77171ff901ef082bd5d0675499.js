17540f86096be9b96f00edeffd5dfe0abe5387f8
mr_darcy <one.akbar@mail.ru>
2022-10-13 16:31:12 +0500

    Updates

---
+++ precache-manifest.815afb77171ff901ef082bd5d0675499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8352ec7486625a0fef89468165b19756",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "bc84aa2a3e6fe6283a0c",
+    "url": "/simple-react-homeworks-master/static/css/main.a54b04ea.chunk.css"
+  },
+  {
+    "revision": "2501a1d00e1a5db8b975",
+    "url": "/simple-react-homeworks-master/static/js/2.6a4daff6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks-master/static/js/2.6a4daff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc84aa2a3e6fe6283a0c",
+    "url": "/simple-react-homeworks-master/static/js/main.0ac3c055.chunk.js"
+  },
+  {
+    "revision": "4d3b136f42fc51341e1d",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.39865212.js"
+  }
+]);
